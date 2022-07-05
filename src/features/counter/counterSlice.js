40cac2e038353d2b0519523c164e68cb346c5509
wilkersoh <plgu@hotmail.com>
2022-07-05 21:06:06 +0800

    add note

--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -1,5 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+/**
+  1. Create Slice
+  2. export reducers's object fn from counterSlice.actions
+  3. export default counterSlice.reducer for store.js
+  4. register your slice into global store
+  5. you can use reducers's object fn from other files.
+    a. useSelector from react-redux to access the global store and the value
+    a. useDispatch from react-redux to trigger the actions into the store
+*/
+
 const initialState = {
 	count: 0,
 };
